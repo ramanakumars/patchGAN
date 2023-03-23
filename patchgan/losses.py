7cbee2ea4a430d5362729730dae8841f5dc8dd63
Ramanakumar Sankar <rsankar@umn.edu>
2023-03-23 15:59:18 -0500

    new trainer module to handle proper discriminator + neptune integration

--- patchgan/losses.py
+++ patchgan/losses.py
@@ -1,21 +1,22 @@
 import torch
 from torch import nn
 
+
 def tversky(y_true, y_pred, beta, batch_mean=True):
-    tp = torch.sum(y_true*y_pred, axis=(1,2,3))
-    fn = torch.sum((1. - y_pred)*y_true, axis=(1,2,3))
-    fp = torch.sum(y_pred*(1. - y_true), axis=(1,2,3))
-    #tversky = reduce_mean(tp)/(reduce_mean(tp) + 
-    #                           beta*reduce_mean(fn) + 
+    tp = torch.sum(y_true * y_pred, axis=(1, 2, 3))
+    fn = torch.sum((1. - y_pred) * y_true, axis=(1, 2, 3))
+    fp = torch.sum(y_pred * (1. - y_true), axis=(1, 2, 3))
+    # tversky = reduce_mean(tp)/(reduce_mean(tp) +
+    #                           beta*reduce_mean(fn) +
     #                           (1. - beta)*reduce_mean(fp))
-    tversky = tp/\
-        (tp + beta*fn + (1. - beta)*fp)
-    
-    norm = torch.sum(y_true, axis=(1,2,3)) + torch.sum(y_pred, axis=(1,2,3)) - tp
+    tversky = tp /\
+        (tp + beta * fn + (1. - beta) * fp)
+
     if batch_mean:
         return torch.mean((1. - tversky))
     else:
         return (1. - tversky)
+
 
 def fc_tversky(y_true, y_pred, beta, gamma=0.75, batch_mean=True):
     smooth = 1
@@ -25,33 +26,35 @@ def fc_tversky(y_true, y_pred, beta, gamma=0.75, batch_mean=True):
     true_pos = torch.sum(y_true_pos * y_pred_pos, axis=(1,2,3))
     false_neg = torch.sum(y_true_pos * (1-y_pred_pos), axis=(1,2,3))
     false_pos = torch.sum((1-y_true_pos)*y_pred_pos, axis=(1,2,3))
-    
+
     answer = (true_pos + smooth)/(true_pos + beta*false_neg + (1-beta)*false_pos + smooth)
     '''
-    tp = torch.sum(y_true*y_pred, axis=(1,2,3))
-    fn = torch.sum((1. - y_pred)*y_true, axis=(1,2,3))
-    fp = torch.sum(y_pred*(1. - y_true), axis=(1,2,3))
-    #tversky = reduce_mean(tp)/(reduce_mean(tp) + 
-    #                           beta*reduce_mean(fn) + 
+    tp = torch.sum(y_true * y_pred, axis=(1, 2, 3))
+    fn = torch.sum((1. - y_pred) * y_true, axis=(1, 2, 3))
+    fp = torch.sum(y_pred * (1. - y_true), axis=(1, 2, 3))
+    # tversky = reduce_mean(tp)/(reduce_mean(tp) +
+    #                           beta*reduce_mean(fn) +
     #                           (1. - beta)*reduce_mean(fp))
-    tversky = (tp+smooth)/\
-        (tp + beta*fn + (1. - beta)*fp + smooth)
+    tversky = (tp + smooth) /\
+        (tp + beta * fn + (1. - beta) * fp + smooth)
 
     focal_tversky_loss = 1 - tversky
-    
+
     if batch_mean:
         return torch.pow(torch.mean(focal_tversky_loss), gamma)
-    else: 
+    else:
         return torch.pow(focal_tversky_loss, gamma)
 
-adversarial_loss = nn.BCELoss() 
+
+# alias
+adv_loss = nn.BCELoss()
+
 
 def generator_loss(generated_img, target_img):
-    #gen_loss = tversky(target_img, generated_img, beta = 0.7)
-    gen_loss = fc_tversky(target_img, generated_img, beta = 0.7, gamma=0.75)
+    gen_loss = fc_tversky(target_img, generated_img, beta=0.7, gamma=0.75)
     return gen_loss
 
-def discriminator_loss(output, label):
-    disc_loss = adversarial_loss(output, label)
-    return disc_loss
 
+def discriminator_loss(output, label):
+    disc_loss = adv_loss(output, label)
+    return disc_loss
