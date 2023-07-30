30f0ce5c11f796e2cef431627a361b5118beb714
Ramanakumar Sankar <rsankar@umn.edu>
2023-07-30 18:41:02 -0500

    adding FloatingForests and COCOStuff datagenerators

--- patchgan/io.py
+++ patchgan/io.py
@@ -1,100 +1,120 @@
+import torch
+from torch.utils.data import Dataset
 import numpy as np
-import netCDF4 as nc
+import glob
+import os
+from torchvision.io import read_image, ImageReadMode
+from torchvision.transforms import Compose, Resize, RandomCrop, RandomHorizontalFlip, RandomVerticalFlip
+from einops import rearrange
+import rasterio
 
 
-class DataGenerator():
-    def __init__(self, nc_file, batch_size, indices=None):
-        self.nc_file = nc_file
+class FloatingForestsDataset(Dataset):
+    augmentation = None
 
-        self.batch_size = batch_size
+    def __init__(self, imgfolder, maskfolder, size=256, augmentation='randomcrop'):
+        self.images = sorted(glob.glob(os.path.join(imgfolder, "*.tif")))
+        self.masks = sorted(glob.glob(os.path.join(maskfolder, "*.tif")))
+        self.size = size
 
-        if indices is not None:
-            self.indices = indices
-            self.ndata = len(indices)
-        else:
-            with nc.Dataset(nc_file, 'r') as dset:
-                self.ndata = int(dset.dimensions['file'].size)
-            self.indices = np.arange(self.ndata)
+        self.image_ids = [int(os.path.basename(image).replace('.tif', '')) for image in self.images]
+        self.mask_ids = [int(os.path.basename(image).replace('.tif', '')) for image in self.masks]
 
-        print(f"Found data with {self.ndata} images")
+        assert np.all(self.image_ids == self.mask_ids), "Image IDs and Mask IDs do not match!"
 
-    def shuffle(self):
-        np.random.shuffle(self.indices)
+        if augmentation == 'randomcrop':
+            self.augmentation = RandomCrop(size=(size, size))
+        elif augmentation == 'randomcrop+flip':
+            self.augmentation = Compose([
+                RandomCrop(size=(size, size)),
+                RandomHorizontalFlip(0.25),
+                RandomVerticalFlip(0.25)
+            ])
+
+        print(f"Loaded {len(self)} images")
 
     def __len__(self):
-        return self.ndata // self.batch_size
+        return len(self.images)
 
     def __getitem__(self, index):
-        batch_indices = self.indices[index *
-                                     self.batch_size:(index + 1) * self.batch_size]
+        image_file = self.images[index]
+        mask_file = self.masks[index]
 
-        if len(batch_indices) < 1:
-            raise StopIteration
+        img = rasterio.open(image_file)
+        img_stacked = np.dstack([img.read(1), img.read(2), img.read(3), img.read(4)])
 
-        return self.get_from_indices(batch_indices)
+        # clean up artifacts in the data
+        img_stacked[np.abs(img_stacked) > 1.e20] = 0
+        img_stacked[np.isnan(img_stacked)] = 0
 
-    def get_from_indices(self, indices):
-        with nc.Dataset(self.nc_file, 'r') as dset:
-            imgs = dset.variables['imgs'][indices,
-                                          :, :, :].astype(float) / 255.
-            mask = dset.variables['mask'][indices, :, :].astype(float)
+        # remove negative signal
+        img_stacked = img_stacked - np.percentile(img_stacked.flatten(), 2)
 
-        return imgs, np.expand_dims(mask, axis=1)
+        norm = np.nansum(img_stacked, axis=-1, keepdims=True)
+        img_stacked = img_stacked / (norm + 1.e-3)
 
-    def get_meta(self, key, index=None):
-        if index is not None:
-            batch_indices = self.indices[index *
-                                         self.batch_size:(index + 1) * self.batch_size]
-        else:
-            batch_indices = self.indices
+        # normalize the image per-pixel
+        img = np.clip(img_stacked, 0, 1)
+        img[~np.isfinite(img)] = 0.
 
-        with nc.Dataset(self.nc_file, 'r') as dset:
-            var = dset.variables[key][batch_indices]
+        # add the mask so we can crop it
+        mask = rasterio.open(mask_file).read(1)
+        mask[mask < 0.] = 0.
+        data_stacked = np.concatenate((img, np.expand_dims(mask, -1)), axis=-1)
+        data_stacked = rearrange(torch.Tensor(data_stacked), "h w c -> c h w")
 
-        return var
+        if self.augmentation is not None:
+            data_stacked = self.augmentation(data_stacked)
+
+        return data_stacked[:4, :, :], data_stacked[4, :, :].unsqueeze(0)
 
 
-class MmapDataGenerator(DataGenerator):
-    def __init__(self, img_file, mask_file, batch_size, indices=None):
-        self.img_file = img_file
-        self.mask_file = mask_file
+class COCOStuffDataset(Dataset):
+    augmentation = None
 
-        self.batch_size = batch_size
+    def __init__(self, imgfolder, maskfolder, labels=[1], size=256, augmentation='resize'):
+        self.images = np.asarray(sorted(glob.glob(os.path.join(imgfolder, "*.jpg"))))
+        self.masks = np.asarray(sorted(glob.glob(os.path.join(maskfolder, "*.png"))))
+        self.size = size
+        self.labels = np.sort(labels)
 
-        self.imgs = np.load(self.img_file, mmap_mode='r')
-        self.mask = np.load(self.mask_file, mmap_mode='r')
+        self.image_ids = [int(os.path.basename(image).replace('.jpg', '')) for image in self.images]
+        self.mask_ids = [int(os.path.basename(image).replace('.png', '')) for image in self.masks]
 
-        self.ndata = len(self.imgs)
+        assert np.all(self.image_ids == self.mask_ids), "Image IDs and Mask IDs do not match!"
 
-        if indices is not None:
-            self.indices = indices
-        else:
-            self.indices = np.arange(self.ndata)
+        if augmentation == 'randomcrop':
+            self.augmentation = Resize(size=(size, size), antialias=None)
+        elif augmentation == 'randomcrop+flip':
+            self.augmentation = Compose([
+                Resize(size=(size, size), antialias=None),
+                RandomHorizontalFlip(0.25),
+                RandomVerticalFlip(0.25),
+            ])
 
-        print(f"Found data with {self.ndata} images")
+        print(f"Loaded {len(self)} images")
 
-    def get_from_indices(self, batch_indices):
-        img = self.imgs[batch_indices, :].astype(float) / 255
-        mask = self.mask[batch_indices, :].astype(float)
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        image_file = self.images[index]
+        mask_file = self.masks[index]
+
+        img = read_image(image_file, ImageReadMode.RGB) / 255.
+        labels = read_image(mask_file, ImageReadMode.GRAY) + 1
+
+        # add the mask so we can crop it
+        data_stacked = torch.cat((img, labels), dim=0)
+
+        if self.augmentation is not None:
+            data_stacked = self.augmentation(data_stacked)
+
+        img = data_stacked[:3, :]
+        labels = data_stacked[3, :]
+
+        mask = torch.zeros((len(self.labels), labels.shape[0], labels.shape[1]))
+        for i, label in enumerate(self.labels):
+            mask[i, labels == label] = 1
 
         return img, mask
-
-
-def create_generators(generator, val_split=0.1, **kwargs):
-    gen = generator(**kwargs)
-
-    ndata = gen.ndata
-
-    print(f"Creating generators from {ndata} images")
-
-    inds = np.arange(ndata)
-    np.random.shuffle(inds)
-
-    val_split_ind = int(ndata * val_split)
-    val_ind = inds[:val_split_ind]
-    training_ind = inds[val_split_ind:]
-
-    train_data = generator(**kwargs, indices=training_ind)
-    val_data = generator(**kwargs, indices=val_ind)
-
-    return train_data, val_data
