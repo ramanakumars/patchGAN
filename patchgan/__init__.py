ba61cb4ca6b718e36706d8e28e32bd938dca24bf
Ramanakumar Sankar <rsankar@umn.edu>
2023-07-30 19:19:48 -0500

    adding versioning

--- patchgan/__init__.py
+++ patchgan/__init__.py
@@ -1,7 +1,8 @@
 from .unet import UNet
 from .disc import Discriminator
 from .trainer import Trainer
+from .version import __version__
 
 __all__ = [
-    'UNet', 'Discriminator', 'Trainer'
+    'UNet', 'Discriminator', 'Trainer', '__version__'
 ]
--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with open(os.path.join(here, 'README.md'), 'r') as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = ''
+
+version = {}
+with open(os.path.join(here, 'patchgan/version.py')) as ver_file:
+    exec(ver_file.read(), version)
 
 setup(
     name='patchGAN',
-    version='0.1',
+    version=version['__version__'],
     description='patchGAN image segmentation model in PyTorch',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='GNU General Public License v3',
     url='https://github.com/ramanakumars/patchGAN',
     author='Kameswara Mantha, Ramanakumar Sankar, Lucy Fortson',
@@ -11,8 +26,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         'numpy>=1.21.0,<1.25.2',
-        'torch>=1.13.0,<=2.0.1',
-        'torchvision>=0.14.0<=0.15.0',
+        'torch>=1.13.0',
+        'torchvision>=0.14.0,<=0.15.0',
         'tqdm>=4.62.3,<=4.65.0',
     ]
 )
