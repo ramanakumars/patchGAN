4f52d44500462a9d904e8da977b29f15393c5529
Ramanakumar Sankar <rsankar@umn.edu>
2023-08-02 19:33:44 -0500

    moving dataset arguments into dataset keyword for training

--- patchgan/train.py
+++ patchgan/train.py
@@ -33,28 +33,29 @@ def patchgan_train():
     with open(args.config_file, 'r') as infile:
         config = yaml.safe_load(infile)
 
-    train_data_paths = config['train_data']
-    val_data_paths = config['validation_data']
+    dataset_params = config['dataset']
+    train_data_paths = dataset_params['train_data']
+    val_data_paths = dataset_params['validation_data']
 
-    size = config['dataset'].get('size', 256)
-    augmentation = config['dataset'].get('augmentation', 'randomcrop')
+    size = dataset_params.get('size', 256)
+    augmentation = dataset_params.get('augmentation', 'randomcrop')
 
     dataset_kwargs = {}
-    if config['dataset']['type'] == 'COCOStuff':
+    if dataset_params['type'] == 'COCOStuff':
         Dataset = COCOStuffDataset
         in_channels = 3
-        labels = config['dataset'].get('labels', [1])
+        labels = dataset_params.get('labels', [1])
         out_channels = len(labels)
         dataset_kwargs['labels'] = labels
     else:
         try:
             spec = importlib.machinery.SourceFileLoader('io', 'io.py')
-            Dataset = spec.load_module().__getattribute__(config['dataset']['type'])
+            Dataset = spec.load_module().__getattribute__(dataset_params['type'])
         except FileNotFoundError:
             print("Make sure io.py is in the working directory!")
             raise
         except (ImportError, ModuleNotFoundError):
-            print(f"io.py does not contain {config['dataset']['type']}")
+            print(f"io.py does not contain {dataset_params['type']}")
             raise
         in_channels = 4
         out_channels = 1
@@ -62,12 +63,13 @@ def patchgan_train():
     train_datagen = Dataset(train_data_paths['images'], train_data_paths['masks'], size=size, augmentation=augmentation, **dataset_kwargs)
     val_datagen = Dataset(val_data_paths['images'], val_data_paths['masks'], size=size, augmentation=augmentation, **dataset_kwargs)
 
-    gen_filts = config['model_params']['gen_filts']
-    disc_filts = config['model_params']['disc_filts']
-    n_disc_layers = config['model_params']['n_disc_layers']
-    activation = config['model_params']['activation']
-    use_dropout = config['model_params'].get('use_dropout', True)
-    final_activation = config['model_params'].get('final_activation', 'sigmoid')
+    model_params = config['model_params']
+    gen_filts = model_params['gen_filts']
+    disc_filts = model_params['disc_filts']
+    n_disc_layers = model_params['n_disc_layers']
+    activation = model_params['activation']
+    use_dropout = model_params.get('use_dropout', True)
+    final_activation = model_params.get('final_activation', 'sigmoid')
 
     train_data = DataLoader(train_datagen, num_workers=args.dataloader_workers, batch_size=args.batch_size, shuffle=True, pin_memory=True)
     val_data = DataLoader(val_datagen, num_workers=args.dataloader_workers, batch_size=args.batch_size, shuffle=True, pin_memory=True)
